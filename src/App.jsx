201d31178eb2db548909bb80446d1759a3cf5055
Arturo1502 <149508454+Arturo1502@users.noreply.github.com>
2024-01-18 01:10:53 -0500

    ajuste y cambio a tailwind

---
+++ src/App.jsx
@@ -0,0 +1,84 @@
+import { useEffect, useState } from "react";
+import Windbnb from "/logo.svg";
+import "./App.css";
+import Navbar from "./components/Navbar"
+import Cards from "./components/Cards";
+
+
+function App() {
+
+
+  const [categorias, setCategories] = useState();
+  const [filteredCategories, setFilteredCategories] = useState([]);
+  const [searchValue, setSearchValue] = useState();
+
+  async function getData() {
+    const fetchData = await fetch("stays.json");
+
+    const datajson = await fetchData.json();
+
+    /* setUsers(datajson); */
+    setCategories(datajson);
+    setFilteredCategories(datajson);
+
+  }
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+
+  const search = () => {
+    const data = categorias.filter((categoria) =>
+      categoria.city.toLowerCase().includes(searchValue.toLowerCase())
+    );
+
+    setFilteredCategories(data);
+  };
+  return (
+    <>
+      <Navbar
+        searchValue={searchValue}
+        setSearchValue={(e) => setSearchValue(e.target.value)}
+        search={search}
+        img={Windbnb}
+
+      />
+
+      <div className="title">
+        <h1 className="text-3xl">
+          <strong>Stays in Finland</strong>
+        </h1>
+        <span className="stays">12+ stays</span>
+      </div>
+
+      <ul className="contenido ">
+        {filteredCategories &&
+          filteredCategories.map((user,id) => (
+
+            <Cards
+              key={id}
+
+              img={user.photo}
+              superHost={user.superHost}
+              type={user.type}
+              rating={user.rating}
+              title={user.title}
+            />
+
+          ))}
+      </ul>
+      <footer>
+        <p>
+          created by{" "}
+          <a href="">
+            <strong>Arturo Alvarez</strong>
+          </a>
+          - devChallenges.io
+        </p>
+      </footer>
+    </>
+  );
+}
+
+export default App;
