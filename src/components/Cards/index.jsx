6b1928235869e6f0ec3ac7658c9a71ba18e683d5
Arturo1502 <149508454+Arturo1502@users.noreply.github.com>
2024-01-20 22:48:02 -0500

    bug fixed superHost

--- src/components/Cards/index.jsx
+++ src/components/Cards/index.jsx
@@ -8,7 +8,7 @@ export default function Cards({ img, superHost, type, beds, rating, title }) {
             <img src={img} alt="Department Photo" className='w-96 h-64 top-[200px] left-[94px] rounded-3xl' />
             <div className='information'>
                 <div className='flex items-center justify-between m-1 '>
-                    <span className={`${superHost} ? " superHost bg-transparent rounded-xl border border-solid border-stone-500 w-[120px] h-[28px] shrink-0 text-stone-500 text-sm"  : "false"}`}>SUPER HOST</span>
+                    <span className={`${superHost} ? " superHost bg-transparent rounded-xl border border-solid border-stone-500 w-[124px] h-[28px] shrink-0 text-stone-500 text-sm"  : "false"}`}>SUPER HOST</span>
                     <p className="text-neutral-500">{`${superHost}` ? `${type}.${beds} beds` : `${type}`}</p>
                     <div className='flex items-center'>
                         <img className='h-4' src={star} alt="star" />
