d82e550fd0797aeeafe55265b155a48af8658660
Arturo1502 <149508454+Arturo1502@users.noreply.github.com>
2024-01-22 01:47:38 -0500

    ajuste de buscador

--- src/components/SearchList/index.jsx
+++ src/components/SearchList/index.jsx
@@ -1,27 +1,26 @@
 import React from "react";
 import './SearchList.css'
-export default function SearchList() {
+
+
+export default function SearchList({ onClick }) {
+    const cities = ["Helsinki, Finland", "Turku, Finland", "Oulu, Finland", "Vaasa, Finland"];
+
     return (
         <>
             <ul className="list flex flex-col text-gray-500 absolute left-[90px]">
-                <div className="flex items-center w-[410px] h-12 hover:bg-gray-200 hover:text-white duration-500 location cursor-pointer">
-                    <span class="material-symbols-outlined">location_on</span>
-                    <li>Helsinki, Finland</li>
-                </div>
-                <div className="flex items-center w-[410px] h-12 hover:bg-gray-200 hover:text-white duration-500 location cursor-pointer">
-                    <span class="material-symbols-outlined">location_on</span>
-                    <li>Turku, Finland</li>
-                </div>
-                <div className="flex items-center w-[410px] h-12 hover:bg-gray-200 hover:text-white duration-500 location cursor-pointer">
-                    <span class="material-symbols-outlined">location_on</span>
-                    <li>Oulu, Finland</li>
-                </div>
-                <div className="flex items-center w-[410px] h-12 hover:bg-gray-200 hover:text-white duration-500 location cursor-pointer">
-                    <span class="material-symbols-outlined">location_on</span>
-                    <li>Vaasa, Finland</li>
-                </div>
-
+                {cities.map((city, index) => (
+                    <div
+                        key={index}
+                        className="flex items-center w-[410px] h-12 hover:bg-gray-200 hover:text-white duration-500 location cursor-pointer"
+                        onClick={() => onClick(city)}
+                    >
+                        <span className="material-symbols-outlined">location_on</span>
+                        <li>{city}</li>
+                    </div>
+                ))}
             </ul>
         </>
     );
 }
+// arreglar el evento onclick
+
